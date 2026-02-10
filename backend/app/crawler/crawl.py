a5c8f57c8ad479bd12f3bbbb662a4f06f8987189
Kush05Bhardwaj <kush2012bhardwaj@gmail.com>
2026-02-10 23:53:33 +0530

    basic crawler

--- backend/app/crawler/crawl.py
+++ backend/app/crawler/crawl.py
@@ -1,1 +1,16 @@
-# collects text
+# crawl web pages
+import requests
+from bs4 import BeautifulSoup
+
+def crawl_url(url):
+    r = requests.get(url, timeout=10)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    for tag in soup.find_all(['script', 'style', 'nav', 'footer', 'header', 'aside']):
+        tag.decompose()
+
+    text = " ".join(soup.stripped_strings)
+    return {
+        "url": url,
+        "text": text
+    }
